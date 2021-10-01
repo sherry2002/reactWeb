59818322c69a492ddd9088e5f3fbb79cfe13139a
sherry2002 <muhammadshehryar93@gmail.com>
2021-10-01 15:53:58 -0700

    Updates

---
+++ precache-manifest.539b92db26b1df29349ce12ddcfedc42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b4a77063d5d2a57c24bb5d1ebb629a",
+    "url": "/reactWeb/index.html"
+  },
+  {
+    "revision": "61d825f33bea767d806d",
+    "url": "/reactWeb/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "bfaa567b17f0e866c4e4",
+    "url": "/reactWeb/static/css/main.d4aba3f9.chunk.css"
+  },
+  {
+    "revision": "61d825f33bea767d806d",
+    "url": "/reactWeb/static/js/2.fd34afc1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactWeb/static/js/2.fd34afc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfaa567b17f0e866c4e4",
+    "url": "/reactWeb/static/js/main.61b15fc2.chunk.js"
+  },
+  {
+    "revision": "54543975225bd6ba95e0",
+    "url": "/reactWeb/static/js/runtime-main.4ef91ad2.js"
+  },
+  {
+    "revision": "43e2463199d9acba1f379eaa084daf65",
+    "url": "/reactWeb/static/media/1.43e24631.jpg"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/reactWeb/static/media/2.57e269d7.jpeg"
+  },
+  {
+    "revision": "082b37229adf8c30d2465d93201379cd",
+    "url": "/reactWeb/static/media/4.082b3722.jpg"
+  }
+]);
